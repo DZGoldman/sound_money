ce344da1199c74441b8d8e48ee07c55eb9c05217
Daniel Goldman <dzgoldman@wesleyan.edu>
2018-11-10 13:18:04 -0500

    add animations, manage all states

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import "./App.css";
 import { ReactMic } from "react-mic";
 import forge from "node-forge";
 import bitcoin from "bitcoinjs-lib";
+import $ from "jquery";
+import QR from "./QR";
+import { Animated } from "react-animated-css";
 
 // var hash = bitcoin.crypto.sha256(Buffer.from('correct horalsdhljkasdfkladhsfse battery staple'))
 // console.log(hash)
@@ -10,7 +13,7 @@ import bitcoin from "bitcoinjs-lib";
 // const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey })
 // console.log(address)
 var hash = "";
-const recordTime = 10;
+const recordTime = 3;
 
 var QRCode = require("qrcode");
 
@@ -24,75 +27,89 @@ class App extends Component {
     shortCountDown: 0,
     mainCountDown: 0,
     // make noise
-    showQRs: false
+    QRs: false,
+    showOffline: false
   };
 
   // phases: start, instr1, instr2, record, qr
   initInstr1 = () => {
     // todo: online status
-    this.setState({
-      phase: 'instr1',
-      showHead: false,
-      nextButtonText: ''
-    }, ()=>{
-      this.setMessage('1. disconnect your internet access (recommended)', 
-      ()=> this.setState({ nextButtonText: 'next'}))
-    })
-  }
+    this.setState(
+      {
+        phase: "instr1",
+        showHead: false,
+        nextButtonText: ""
+      },
+      () => {
+        this.setMessage(
+          "1. disconnect your internet access (recommended)",
+          () => this.setState({ nextButtonText: "next" })
+        );
+      }
+    );
+  };
 
   initInstr2 = () => {
     // todo: online status
-    this.setState({
-      phase: 'instr2',
-      nextButtonText: ''
-    }, ()=>{
-      this.setMessage('2. ensure audio is connected; the red line should be active', 
-      ()=> this.setState({ nextButtonText: 'next', recording: true}))
-    })
-  }
+    this.setState(
+      {
+        phase: "instr2",
+        nextButtonText: ""
+      },
+      () => {
+        this.setMessage(
+          "2. ensure audio is connected; the red line should be active",
+          () => this.setState({ nextButtonText: "next", recording: true })
+        );
+      }
+    );
+  };
 
   initRecording = () => {
-    this.setState({
-      phase: 'record',
-      message: '',
-      nextButtonText: '',
-      recording: false
-    }, ()=>{
-      var count = 3;
-      this.setState({
-        shortCountDown: count
-      })
-      const intID = window.setInterval(()=>{
-        count -- 
+    this.setState(
+      {
+        phase: "record",
+        message: "",
+        nextButtonText: "",
+        recording: false
+      },
+      () => {
+        var count = 3;
         this.setState({
           shortCountDown: count
-        })
+        });
+        const intID = window.setInterval(() => {
+          count--;
+          this.setState({
+            shortCountDown: count
+          });
 
-        if (count == 0){
-          window.clearInterval(intID)
-          this.startRecording()
-        }
-      }, 1000)
-    })
-  }
+          if (count == 0) {
+            window.clearInterval(intID);
+            this.startRecording();
+          }
+        }, 1000);
+      }
+    );
+  };
   componentDidMount = () => {
     window.r = this;
-    // this.startRecording()
 
-    var canvas = document.getElementById('canvas')
+    $(() => {
+      window.addEventListener("offline", this.goOffline, false);
+      // window.setTimeout(()=>{
+      //   $('#next-button').animate({opacity: 1}, 1000)
 
-    QRCode.toCanvas(canvas, '12Yr17SCr5TuDzbr56s36nY9qBYYpM5Lm2', function (error) {
-      if (error) console.error(error)
-      console.log('success!');
-    })
-    // window.print();
-    window.addEventListener('offline', this.goOffline, false)
+      // }, 500)
+    });
   };
 
   goOffline = () => {
-    console.log('went offline')
-    alert('asdf')
-  }
+    console.log("went offline");
+    this.setState({
+      showOffline: true
+    });
+  };
 
   sha256 = str => {
     var md = forge.md.sha256.create();
@@ -103,18 +120,23 @@ class App extends Component {
     console.log("finalizing");
     this.setState(
       {
-        recording: false
+        recording: false,
+        phase: "endRecord"
       },
       () => {
         hash = hash + +new Date() + Math.random();
         var privk = bitcoin.crypto.sha256(Buffer.from(hash));
         // console.log(privk)
         const keyPair = bitcoin.ECPair.fromPrivateKey(privk);
-        console.log("privkey", keyPair.toWIF());
         const { address } = bitcoin.payments.p2pkh({
           pubkey: keyPair.publicKey
         });
-        console.log("pub address compressed", address);
+        this.setState({
+          QRs: {
+            private: keyPair.toWIF(),
+            address
+          }
+        });
 
         // var md = forge.md.sha256.create();
         // md.update(hash);
@@ -123,7 +145,7 @@ class App extends Component {
     );
   };
   onData = blob => {
-    if (this.state.phase != 'record') return
+    if (this.state.phase != "record") return;
     var reader = new FileReader();
     reader.addEventListener("loadend", () => {
       var buf = reader.result;
@@ -146,13 +168,11 @@ class App extends Component {
     });
 
     window.setTimeout(() => {
-      console.log("stopping recording");
-      this.setState({
-        recording: false
-      });
+      this.finalize();
     }, recordTime * 1000);
   };
 
+  initStopRecording() {}
   setMessage = (msg, then) => {
     this.state.message && this.setState({ message: "", showNext: false });
     var i = 0;
@@ -167,38 +187,57 @@ class App extends Component {
   };
 
   nextClick = () => {
-    const {phase} = this.state
-    if (phase == 'start'){
-      this.initInstr1()
-    } else if (phase == 'instr1'){
-      this.initInstr2()
-    } else if (phase == 'instr2') {
-      this.initRecording()
+    const { phase } = this.state;
+    if (phase == "start") {
+      this.initInstr1();
+    } else if (phase == "instr1") {
+      this.initInstr2();
+    } else if (phase == "instr2") {
+      this.initRecording();
     }
-  }
-
-  
+  };
 
   render() {
-    const {showHead, nextButtonText, shortCountDown, phase, recording} = this.state
+    const {
+      showHead,
+      nextButtonText,
+      shortCountDown,
+      phase,
+      recording,
+      QRs,
+      showOffline
+    } = this.state;
     return (
       <div className="App">
-        <canvas id="canvas"></canvas>
-
-   
-          <div id="header-container">
-            <div id="header">{showHead && "sound money"}</div>
-            <div id="sub">{showHead && "paper wallet generator from audio entropy"}</div>
+        {QRs && <QR QRs={QRs} />}
+        <div id="header-container">
+          <div id="header">{showHead && "sound money"}</div>
+          <div id="sub">
+            {showHead && "paper wallet generator from audio entropy"}
           </div>
-        
+        </div>
+        {showOffline && <div id="offline">you went offline (apparantly)</div>}
         <div id="message">{this.state.message}</div>
-      {this.state.nextButtonText && <div onClick={this.nextClick} id="next-button">{this.state.nextButtonText}</div> }
-        {this.state.showNext && <button onClick={this.nextPhase}>next</button>}
+        <div id="next-button-wrapper">
+          {this.state.nextButtonText && (
+            <Animated
+              animationIn="fadeIn"
+              animationOut="fadeOut"
+              isVisible={true}
+            >
+              <div onClick={this.nextClick} id="next-button">
+                <span>{this.state.nextButtonText}</span>
+              </div>
+            </Animated>
+          )}
+        </div>
         <div>{shortCountDown > 0 && shortCountDown}</div>
-        <div id='make-noise'>{recording && phase == 'record' && "make noise"}</div>
+        <div id="make-noise">
+          {recording && phase == "record" && "make noise"}
+        </div>
         <ReactMic
           record={this.state.recording} // defaults -> false.  Set to true to begin recording
-          onStop={this.finalize} // callback to execute when audio stops recording
+          // onStop={this.finalize} // callback to execute when audio stops recording
           onData={this.onData} // callback to execute when chunk of audio data is available
           strokeColor={"red"} // sound wave color
           backgroundColor={"black"} // background color
